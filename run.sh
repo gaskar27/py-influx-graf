003ef94ac427c18a4e4fd6e4f56083e0fdb81c9d
gaskar <anjarazafinjato@gmail.com>
2026-06-22 11:37:16 +0300

    feat(run): add start/stop/down commands

--- run.sh
+++ run.sh
@@ -1,5 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -euo pipefail
+set -e
 
-docker compose up -d
+if [ -z $1 ]; then
+    echo "Usage: $0 <args>"
+    echo "args:"
+    echo "      start : Run the application"
+    echo "      stop  : Stop the application"
+    echo "      down  : Stop the application and remove data"
+    exit 1
+fi
+
+if [ "$1" = "down" ]; then
+    docker compose down -v
+
+    if [ -d ~/.influxdb3/data ]; then
+        rm -rf ~/.influxdb3/data
+    fi
+
+    exit 0
+fi
+
+if [ "$1" = "stop" ]; then
+    docker compose stop
+    exit 0
+fi
+
+if [ "$1" = "start" ]; then
+    mkdir -p ~/.influxdb3/data
+    docker compose up -d
+    exit 0
+fi
