32fd7706b3d61be2713b59fe2cf3a5a6ffdfea77
gaskar <anjarazafinjato@gmail.com>
2026-06-22 14:32:00 +0300

    fix(run): handle args safely and add aliases

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "Usage: $0 <args>"
     echo "args:"
     echo "      start : Run the application"
@@ -11,23 +11,25 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-if [ "$1" = "down" ]; then
-    docker compose down -v
+case "$1" in
+    "start" | "s")
+        docker compose up -d
+        ;;
 
-    if [ -d ~/.influxdb3/data ]; then
-        rm -rf ~/.influxdb3/data
-    fi
+    "stop" | "st")
+        docker compose stop
+        ;;
 
-    exit 0
-fi
+    "down" | "d")
+        docker compose down -v
+        ;;
 
-if [ "$1" = "stop" ]; then
-    docker compose stop
-    exit 0
-fi
-
-if [ "$1" = "start" ]; then
-    mkdir -p ~/.influxdb3/data
-    docker compose up -d
-    exit 0
-fi
+    *)
+        echo "Usage: $0 <args>"
+        echo "args:"
+        echo "      start : Run the application"
+        echo "      stop  : Stop the application"
+        echo "      down  : Stop the application and remove data"
+        exit 1
+        ;;
+esac
