4b141bf98b75e8d3330370b0b3d5577dc5e3be04
gaskar <anjarazafinjato@gmail.com>
2026-07-27 13:16:59 +0300

    feat: add main script to run collectors
    This commit introduces `main.py` which orchestrates the execution of
    various collector scripts. This centralizes the process and provides
    a single entry point for cron.

---
+++ requirements/collector/app/main.py
@@ -0,0 +1,14 @@
+import urllib3
+import subprocess
+
+
+# Désactiver les avertissements SSL pour les certificats auto-signés
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+scripts = ["powerstore.py", "unity.py", "vsphere.py"]
+
+for script in scripts:
+    print(f"Running {script}...")
+    subprocess.run(["python3", script])
+
+print("All scripts have been executed.")
