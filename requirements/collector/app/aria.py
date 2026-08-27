73449111fc0fd2936477a7a238c4e5b7bddd278e
gaskar <anjarazafinjato@gmail.com>
2026-08-27 15:05:06 +0300

    feat(aria): add vm lifecycle data collection
    This commit introduces a new measurement, `vm_lifecycle`, to track
    virtual machine creation and deletion events. It utilizes the InfluxDB
    Point object for writing data.

--- requirements/collector/app/aria.py
+++ requirements/collector/app/aria.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 
 import requests
+from influxdb_client_3 import Point
+
 from influx_writer import writer as db
 from utils import get_secrets
 
@@ -10,8 +12,8 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Aria
-ARIA_RESOURCE_ID1 = os.getenv("ARIA_RESOURCE_ID1")
-ARIA_RESOURCE_ID2 = os.getenv("ARIA_RESOURCE_ID2")
+ARIA_RESOURCE_ID1 = str(os.getenv("ARIA_RESOURCE_ID1"))
+ARIA_RESOURCE_ID2 = str(os.getenv("ARIA_RESOURCE_ID2"))
 MAP_ID1 = os.getenv("MAP_ID1")
 MAP_ID2 = os.getenv("MAP_ID2")
 
@@ -29,6 +31,7 @@ class AriaCollector:
         self.begin = self.end - (30 * 24 * 60 * 60 * 1000)
         self.names_map = { ARIA_RESOURCE_ID1 : MAP_ID1, ARIA_RESOURCE_ID2 : MAP_ID2 }
         self.lines = [] # list of influxDB line
+        self.points = [] # list of influxDB point
 
     def __get_token(self, payload: dict):
         url = f"{self.base_url}/auth/token/acquire"
@@ -73,6 +76,50 @@ class AriaCollector:
 
         return lp
 
+    def __get_vm_destroy_date(self, id:str):
+        url = f"{self.base_url}/resources/{id}/stats/latest"
+        try:
+            response = self.session.get(url)
+            if response.status_code != 200:
+                print(f" Error {response.status_code}:", response.text)
+            response.raise_for_status()
+            data = response.json()
+            values = data.get("values", [])
+            if not values:
+                return None
+
+            stats = values[0].get("stat-list", {}).get("stat", [])
+            if not stats:
+                return None
+
+            timestamps = stats[0].get("timestamps", [])
+            if not timestamps:
+                return None
+            return timestamps[0] # the result is in millisecond
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def __convert_to_influx_point(self, data, instance_id):
+        resource_name = str(self.names_map.get(instance_id, instance_id) if self.names_map else instance_id)
+        for item in data:
+            vm_name = item.get("resourceKey", {}).get("name")
+            created_at = item.get("creationTime")
+            is_deleted = False
+            vm_id = item.get("identifier")
+            destroyed_at = None
+            for state in item.get("resourceStatusStates", []):
+                if state.get("resourceState") == "NOT_EXISTING":
+                    is_deleted = True
+                    break
+
+            if not is_deleted:
+                destroyed_at = self.__get_vm_destroy_date(vm_id)
+            point = (Point("vm_lifecycle").time(created_at).tag("datacenter", resource_name)
+                     .tag("name", vm_name).tag("id", vm_id).field("is_deleted", is_deleted)
+                     .field("created_at", created_at)).field("destroyed_at", destroyed_at)
+            self.points.append(point)
+
     def get_total_vms(self):
         url = f"{self.base_url}/resources/stats/latest?resourceId={ARIA_RESOURCE_ID1}&resourceId={ARIA_RESOURCE_ID2}&statKey=summary|total_number_vms"
         try:
@@ -82,6 +129,22 @@ class AriaCollector:
             response.raise_for_status()
             lp = self.__influx_line_protocol(response.json(), "vm_inventory")
             self.lines.extend(lp)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def get_vms_resource_stat(self, adapter_instance_id: str):
+        url = f"{self.base_url}/resources/query"
+        payload = {
+            "resourceKind" : [ "VirtualMachine" ],
+            "adapterInstanceId" : [ adapter_instance_id ],
+        }
+        try:
+            response = self.session.post(url, json=payload)
+            if response.status_code != 200:
+                print(f" Error {response.status_code}:", response.text)
+            response.raise_for_status()
+            if response.json():
+                self.__convert_to_influx_point(response.json().get("resourceList", []), adapter_instance_id)
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
@@ -138,6 +201,8 @@ class AriaCollector:
         self.get_total_vms()
         self.aria_vmop()
         self.aria_cpu_mem()
+        self.get_vms_resource_stat(ARIA_RESOURCE_ID1)
+        self.get_vms_resource_stat(ARIA_RESOURCE_ID2)
 
 
 if __name__ == "__main__":
@@ -155,6 +220,9 @@ if __name__ == "__main__":
     print("Writing to InfluxDB...")
     for line in aria.lines:
         db.write_line(line)
+
+    for point in aria.points:
+        db.write_point(point)
 
     db.close()
     print("--- End ---")
